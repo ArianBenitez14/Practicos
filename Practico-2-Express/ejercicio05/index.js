7cca71ed0b27d903a7373c11a476dbadd6001739
ArianBenitez14 <arianbe14@gmail.com>
2025-03-21 08:45:40 -0300

    commit

--- Practico-2-Express/ejercicio05/index.js
+++ Practico-2-Express/ejercicio05/index.js
@@ -1,29 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let correos = [];
 
-app.get("/", (req, res) =>
-  res.send("Accediste a la base de correos de la empresa.")
+app.get('/', (req, res) =>
+  res.send('Accediste a la base de correos de la empresa.')
 );
 
-app.get("/subscribe", (req, res) => {
-  res.sendFile(__dirname + "/subscribe.html");
+app.get('/subscribe', (req, res) => {
+  res.sendFile(__dirname + '/subscribe.html');
 });
 
-app.get("/subscribe", (req, res) => {
+app.post('/subscribe', (req, res) => {
   const { email } = req.body;
 
   if (correos.includes(email)) {
-    return res.send("¡Correo ya existente!");
+    return res.send('¡Correo ya existente!');
   } else {
-    correos.push({ correo: email });
-    res.send("¡Bienvenido a Transamerican!");
+    correos.push(email);
+    res.send('¡Bienvenido a MercadoLibre!');
   }
 });
 
 app.listen(8000, () => {
-  console.log("http://localhost:8000");
+  console.log('http://localhost:8000');
 });
